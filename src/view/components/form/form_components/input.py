979bd2340749c07fad6f31d9b0782f7dca5c950d
Retr0327 <philcoke35@gmail.com>
2022-07-01 01:31:42 +0800

    Add input

--- src/view/components/form/form_components/input.py
+++ src/view/components/form/form_components/input.py
@@ -4,6 +4,10 @@ from typing import Optional
 
 def add_input(
     title: str, placeholder: Optional[str] = "", max_chars: Optional[int] = None
-):
+) -> str:
     """The add_input function adds a input field."""
-    return st.text_input(title, placeholder=placeholder, max_chars=max_chars)
+    return st.text_input(
+        title,
+        placeholder=placeholder,
+        max_chars=max_chars,
+    )
