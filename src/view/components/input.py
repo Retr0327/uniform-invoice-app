1e6eb2afd55483860b040a1e86fe5c2ef48a79ae
Retr0327 <philcoke35@gmail.com>
2022-06-30 20:19:21 +0800

    Add input

---
+++ src/view/components/input.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from typing import Optional
+
+
+def add_input(
+    title: str, placeholder: Optional[str] = "", max_chars: Optional[int] = None
+):
+    """The add_input function adds a input field."""
+    return st.text_input(title, placeholder=placeholder, max_chars=max_chars)
