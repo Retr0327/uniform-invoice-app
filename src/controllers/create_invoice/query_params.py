0ef9fa213fb565cfa67842bb147213b983339a63
Retr0327 <philcoke35@gmail.com>
2022-07-02 15:16:03 +0800

    Add query_params

---
+++ src/controllers/create_invoice/query_params.py
@@ -0,0 +1,44 @@
+from typing import Dict, List
+from dataclasses import dataclass
+
+
+@dataclass
+class QueryParams:
+    """The QueryParams object creates the data which will be inserted to the database."""
+
+    month: str
+    year: str
+    data: List
+
+    def __post_init__(self) -> None:
+        self.prize_data, self.claiming_data = self.data
+
+    def update_prize_key(self, value: Dict[str, str]) -> Dict[str, str]:
+        """The update_prize_key method adds two new keys, `month` and `year`.
+
+        Args:
+            value: one of the items in `self.prize_data`
+
+        Returns:
+            a dict: {
+                'type': 'first',
+                'prize': '16525386',
+                'month': '3',
+                'year': 111
+            }
+        """
+        value["month"] = self.month
+        value["year"] = self.year
+        return value
+
+    def flat_prize_data(self, value: Dict[str, str]) -> Dict[str, str]:
+        """The flat_prize_data method flats the `self.prize_data`."""
+        for item in value if isinstance(value, list) else (value,):
+            return item
+
+    def build_invoice(self):
+        return (self.month, self.year, *self.claiming_data)
+
+    def build_prize(self):
+        flatted_list = map(self.flat_prize_data, self.prize_data)
+        return list(map(self.update_prize_key, flatted_list))
