7260766a61b8c25024dbeb95469b61175b1f9db6
Retr0327 <philcoke35@gmail.com>
2022-07-02 15:51:14 +0800

    Update form_controller

--- src/view/components/form/form.py
+++ src/view/components/form/form.py
@@ -1,7 +1,9 @@
+from typing import Union
 from .form_components import add_input, add_select
 
 
-def form_controller(type: str, **kwargs) -> str:
+def form_controller(type: str, **kwargs) -> Union[str, int]:
+    """The form_controller function builds a form component based on a given `type`."""
     form_factories = {"input": add_input, "select": add_select}
 
     return form_factories[type](**kwargs)
