2a625b5a492b06701a52b2a61bb1468bfb61321f
Retr0327 <philcoke35@gmail.com>
2022-07-02 15:50:48 +0800

    Update params

--- src/view/components/form/form_components/select.py
+++ src/view/components/form/form_components/select.py
@@ -1,5 +1,7 @@
 import streamlit as st
+from typing import Optional, Union
 
 
-def add_select(title: str, options: tuple) -> str:
-    return st.selectbox(title, options)
+def add_select(title: str, options: tuple, index: Optional[int] = 0) -> Union[str, int]:
+    """The add_select function adds a select field."""
+    return st.selectbox(title, options, index=index)
