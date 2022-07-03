a6d37636d261ef3f0717486319aaab94a82dd1c0
Retr0327 <philcoke35@gmail.com>
2022-07-03 14:17:51 +0800

    Add session

--- src/view/containers/prize_table/prize_table.py
+++ src/view/containers/prize_table/prize_table.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from typing import List
 from .table import Table
 from streamlit.delta_generator import DeltaGenerator
 
 
-def create_prize_table(data: List) -> DeltaGenerator:
-    claiming_date, prize_data = data
+def create_prize_table() -> DeltaGenerator:
+    claiming_date, prize_data = st.session_state["invoice_data"]
     start, end = claiming_date[0]
 
-    st.header(f"領獎日期{start} 至 {end}")
+    st.header(f"領獎日期 {start} 至 {end}")
     return st.table(Table(prize_data).create())
