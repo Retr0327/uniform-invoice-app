cd122d37a34b2cfbac5ef6d51a6eba4cfdd635be
Retr0327 <philcoke35@gmail.com>
2022-07-07 15:55:29 +0800

    Update app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,13 @@
 import asyncio
 import streamlit as st
+from models import init_db
+from config import make_db_dir, DB_PATH
 from view import create_invoice_form, create_prize_table, create_redemption
+
+
+if not DB_PATH.is_file():
+    make_db_dir()
+    init_db()
 
 
 def run_app() -> None:
